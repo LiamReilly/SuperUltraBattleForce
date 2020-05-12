5fa698131e4577d784dcc3b56315a408a4f8fd88
aroy99 <aaron.roy99@yahoo.com>
2020-05-12 18:15:46 -0400

    Fixed case issue with the healthbar files by forcing git to recognize it

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider healthBar;
+    public PlayerBase playerHealth;
+    //public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        healthBar = GetComponent<Slider>();
+        
+    }
+
+
+    public void SetHealth(float hp)
+    {
+        healthBar.value = hp;
+    }
+    public void SetUp(PlayerBase play)
+    {
+        //player = play;
+        playerHealth = play;
+        healthBar.maxValue = playerHealth.maxHealth;
+        //healthBar.maxValue = 100f;
+        healthBar.value = healthBar.maxValue;
+    }
+}
